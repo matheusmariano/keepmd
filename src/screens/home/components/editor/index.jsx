29f7b62199ba8cccc9603698f8e3eb43b1dba87c
Matheus Mariano <vmatheus.mariano@gmail.com>
2019-03-15 02:13:45 -0300

    Convert components to stateless

--- src/screens/home/components/editor/index.jsx
+++ src/screens/home/components/editor/index.jsx
@@ -1,67 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import AceEditor from 'react-ace';
 import Markdown from 'react-markdown';
 import 'brace/mode/markdown';
 import 'brace/theme/tomorrow';
 
-class Editor extends Component {
-  static get propTypes() {
-    return {
-      className: PropTypes.string,
-    };
-  }
+const Editor = ({ className }) => {
+  const [content, setContent] = useState('');
 
-  static get defaultProps() {
-    return {
-      className: '',
-    };
-  }
+  const contentDidChange = (value) => {
+    setContent(value);
+  };
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      content: '',
-    };
-  }
-
-  setContentState(content) {
-    this.setState({
-      content,
-    });
-  }
-
-  contentDidChange(content) {
-    this.setContentState(content);
-  }
-
-  render() {
-    const { content } = this.state;
-    const { className } = this.props;
-
-    return (
-      <div className={`columns is-gapless ${className}`}>
-        <div className="column is-half">
-          <AceEditor
-            width="100%"
-            height="100%"
-            mode="markdown"
-            onChange={value => this.contentDidChange(value)}
-            showPrintMargin={false}
-            tabSize={2}
-            theme="tomorrow"
-            value={content}
-          />
-        </div>
-        <div className="column is-half">
-          <div className="content has-margin-lg">
-            <Markdown source={content} />
-          </div>
+  return (
+    <div className={`columns is-gapless ${className}`}>
+      <div className="column is-half">
+        <AceEditor
+          width="100%"
+          height="100%"
+          mode="markdown"
+          onChange={contentDidChange}
+          showPrintMargin={false}
+          tabSize={2}
+          theme="tomorrow"
+          value={content}
+        />
+      </div>
+      <div className="column is-half">
+        <div className="content has-margin-lg">
+          <Markdown source={content} />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Editor.propTypes = {
+  className: PropTypes.string,
+};
+
+Editor.defaultProps = {
+  className: '',
+};
 
 export default Editor;
--- src/screens/home/components/navbar/index.jsx
+++ src/screens/home/components/navbar/index.jsx
@@ -1,76 +1,62 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import applyClasses from 'classnames';
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props);
+const Navbar = () => {
+  const [expanded, setExpanded] = useState(false);
 
-    this.state = {
-      expanded: false,
-    };
-  }
+  const toggle = () => {
+    setExpanded(!expanded);
+  };
 
-  toggle() {
-    this.setState(
-      state => ({
-        expanded: !state.expanded,
-      }),
-    );
-  }
-
-  render() {
-    const { expanded } = this.state;
-
-    return (
-      <FormattedMessage id="home.navigation.name">
-        {navigationName => (
-          <nav
-            aria-label={navigationName}
-            className="navbar is-primary"
-          >
-            <div className="navbar-brand">
-              <div className="navbar-item">
-                Keepmd
-              </div>
-              <FormattedMessage id="home.navigation.menu">
-                {menu => (
-                  <button
-                    aria-label={menu}
-                    aria-expanded={expanded}
-                    className="button is-primary navbar-burger burger"
-                    onClick={() => this.toggle()}
-                    type="button"
-                  >
-                    <span aria-hidden />
-                    <span aria-hidden />
-                    <span aria-hidden />
-                  </button>
-                )}
-              </FormattedMessage>
+  return (
+    <FormattedMessage id="home.navigation.name">
+      {navigationName => (
+        <nav
+          aria-label={navigationName}
+          className="navbar is-primary"
+        >
+          <div className="navbar-brand">
+            <div className="navbar-item">
+              Keepmd
             </div>
-            <div
-              className={
-                applyClasses('navbar-menu', {
-                  'is-active': expanded,
-                })
-              }
-            >
-              <div className="navbar-end">
-                <a
-                  className="navbar-item"
-                  href="https://github.com/matheusmariano/keepmd"
-                  target="__blank"
+            <FormattedMessage id="home.navigation.menu">
+              {menu => (
+                <button
+                  aria-label={menu}
+                  aria-expanded={expanded}
+                  className="button is-primary navbar-burger burger"
+                  onClick={toggle}
+                  type="button"
                 >
-                  GitHub
-                </a>
-              </div>
+                  <span aria-hidden />
+                  <span aria-hidden />
+                  <span aria-hidden />
+                </button>
+              )}
+            </FormattedMessage>
+          </div>
+          <div
+            className={
+              applyClasses('navbar-menu', {
+                'is-active': expanded,
+              })
+            }
+          >
+            <div className="navbar-end">
+              <a
+                className="navbar-item"
+                href="https://github.com/matheusmariano/keepmd"
+                target="__blank"
+              >
+                GitHub
+              </a>
             </div>
-          </nav>
-        )}
-      </FormattedMessage>
-    );
-  }
-}
+          </div>
+        </nav>
+      )}
+    </FormattedMessage>
+  );
+};
 
 export default Navbar;
